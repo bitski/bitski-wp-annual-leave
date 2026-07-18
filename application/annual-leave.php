16ac0deb5e6fa7732dc8a8accf2307f8f3250a98
bitski <18152029+bitski@users.noreply.github.com>
2026-07-18 16:32:19 +0200

    refactor: introduce annual leave callout view model

--- application/annual-leave.php
+++ application/annual-leave.php
@@ -50,76 +50,40 @@ function praxis_eichholz_annual_leave_render_accordion(): string
  */
 function praxis_eichholz_annual_leave_render_callout(): string
 {
-    $annual_leave_data         = praxis_eichholz_annual_leave_data();
-    $current_annual_leave_data = null;
-    $annual_leave_callout      = '';
+    $current_annual_leave_data = praxis_eichholz_annual_leave_callout_data();
 
-    $current_date = date('Y-m-d');
-
-    foreach ($annual_leave_data as $data) {
-        if (
-            $current_date >= $data['dates']['start'] &&
-            $current_date <= $data['dates']['end']
-        ) {
-            $current_annual_leave_data = $data;
-            break;
-        }
-    }
-
-    if ($current_annual_leave_data !== null) {
-        echo "<pre>";
-        var_dump($current_annual_leave_data);
-        echo "</pre>";
-
-        ob_start();
-        $start_object = DateTime::createFromFormat(
-            'Y-m-d',
-            $current_annual_leave_data['dates']['start'],
-        );
-        $end_object   = DateTime::createFromFormat(
-            'Y-m-d',
-            $current_annual_leave_data['dates']['end'],
-        );
-
-        if (
-            ! ($start_object instanceof DateTime) ||
-            ! ($end_object instanceof DateTime)
-        ) {
-            ob_end_clean();
-
-            return $annual_leave_callout;
-        } ?>
-        <br>
-        <p><strong>Die Praxis ist im Moment geschlossen, da wir gerade im Urlaub
-                sind:</strong></p>
-        <p>
-            <time datetime="<?php
-            echo esc_attr($start_object->format('Y-m-d')); ?>">
-                <?php
-                echo esc_html($start_object->format('d.m.Y')); ?></time>
-            &ndash;
-            <time datetime="<?php
-            echo esc_attr($end_object->format('Y-m-d')); ?>">
-                <?php
-                echo esc_html($end_object->format('d.m.Y')); ?>
-            </time>
-        </p>
-        <p>
+    ob_start();
+    $start_object = new DateTimeImmutable($current_annual_leave_data['dates']['start']);
+    $end_object   = new DateTimeImmutable($current_annual_leave_data['dates']['end']); ?>
+    <br>
+    <p><strong>Die Praxis ist im Moment geschlossen, da wir gerade im Urlaub
+            sind:</strong></p>
+    <p>
+        <time datetime="<?php
+        echo esc_attr($start_object->format('Y-m-d')); ?>">
             <?php
-
-
-            ?>
-        </p>
+            echo esc_html($start_object->format('d.m.Y')); ?></time>
+        &ndash;
+        <time datetime="<?php
+        echo esc_attr($end_object->format('Y-m-d')); ?>">
+            <?php
+            echo esc_html($end_object->format('d.m.Y')); ?>
+        </time>
+    </p>
+    <p>
         <?php
-        $annual_leave_callout = ob_get_clean();
-    }
 
-    return $annual_leave_callout;
+
+        ?>
+    </p>
+    <?php
+    return ob_get_clean();
 }
 
 function praxis_eichholz_annual_leave_accordion_data(): array
 {
-    $annual_leave_accordion_data = praxis_eichholz_annual_leave_data();
+    $annual_leave_data = praxis_eichholz_annual_leave_data();
+    $annual_leave_accordion_data = $annual_leave_data;
 
     usort($annual_leave_accordion_data, static function ($a, $b) {
         return strtotime($a['dates']['start']) <=>
@@ -127,7 +91,7 @@ function praxis_eichholz_annual_leave_accordion_data(): array
     });
 
     foreach ($annual_leave_accordion_data as $key => $data) {
-        // Checks if the start and end dates are valid DateTimeImmutable objects.
+        // Removes entries with invalid date periods.
         $start_object = DateTimeImmutable::createFromFormat(
             'Y-m-d',
             $data['dates']['start'],
@@ -147,3 +111,48 @@ function praxis_eichholz_annual_leave_accordion_data(): array
 
     return $annual_leave_accordion_data;
 }
+
+/**
+ *
+ */
+function praxis_eichholz_annual_leave_callout_data(): array
+{
+    $annual_leave_data = praxis_eichholz_annual_leave_data();
+    $annual_leave_callout_data = [];
+
+    $current_date = date('Y-m-d');
+
+    foreach ($annual_leave_data as $data) {
+        // Skips entries with invalid date periods.
+        $start_object = DateTimeImmutable::createFromFormat(
+            'Y-m-d',
+            $data['dates']['start'],
+        );
+        $end_object   = DateTimeImmutable::createFromFormat(
+            'Y-m-d',
+            $data['dates']['end'],
+        );
+
+        if (
+            ! ($start_object instanceof DateTimeImmutable) ||
+            ! ($end_object instanceof DateTimeImmutable)
+        ) {
+            continue;
+        }
+
+        if (
+            $current_date >= $data['dates']['start'] &&
+            $current_date <= $data['dates']['end']
+        ) {
+            $annual_leave_callout_data = $data;
+            break;
+        }
+    }
+
+    if ($annual_leave_callout_data !== []) {
+        // TODO: Add substitute information.;
+    }
+
+    return $annual_leave_callout_data;
+}
+
